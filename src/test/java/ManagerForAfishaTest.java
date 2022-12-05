0ef3af92d1ed6f0b8086c2a1166d9b5ad5e11b12
Pavel Matveev <reus1896@gmail.com>
2022-12-05 20:50:18 +0300

    add eauals limit

--- src/test/java/ManagerForAfishaTest.java
+++ src/test/java/ManagerForAfishaTest.java
@@ -46,6 +46,22 @@ public class ManagerForAfishaTest {
 }
 
     @Test
+    public void testEqualslimit() {
+        ManagerForAfisha afisha = new ManagerForAfisha(5);
+        afisha.addMovie("movie1");
+        afisha.addMovie("movie2");
+        afisha.addMovie("movie3");
+        afisha.addMovie("movie4");
+        afisha.addMovie("movie5");
+
+
+        String[] expected = {"movie1", "movie2", "movie3", "movie4","movie5"};
+        String[] actual = afisha.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
     public void testMorelimit() {
         ManagerForAfisha afisha = new ManagerForAfisha(5);
         afisha.findMovieLimit("movie1");
